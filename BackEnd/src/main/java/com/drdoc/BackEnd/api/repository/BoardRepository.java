bd7a7642c7341aeba965451fb9f566ac5e57ca77
윤지환 <dddalang4@naver.com>
2022-09-14 17:08:16 +0900

    :sparkles: 커뮤니티 게시글 전체 조회 기능 구현

--- BackEnd/src/main/java/com/drdoc/BackEnd/api/repository/BoardRepository.java
+++ BackEnd/src/main/java/com/drdoc/BackEnd/api/repository/BoardRepository.java
@@ -1,9 +1,11 @@
 package com.drdoc.BackEnd.api.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.drdoc.BackEnd.api.domain.Board;
 
 public interface BoardRepository extends JpaRepository<Board, Integer> {
-
+	Page<Board> findByTypeId(int typeId, Pageable pageable);
 }
