c7c48acc22e73929daf583e69083c4b7f7046004
윤지환 <dddalang4@naver.com>
2022-09-16 11:33:09 +0900

    :wrench: 회원가입 기능 수정
    * 프로필 사진 업로드 추가
    * @Value 검증을 통해 잘못된 입력 값을 받을 시 발생하는 MethodArgumentNotValidException에 대한
    예외 처리를 위해 ExceptionHandler에 해당 예외를 처리하는 로직 추가

--- BackEnd/src/main/java/com/drdoc/BackEnd/api/controller/UserController.java
+++ BackEnd/src/main/java/com/drdoc/BackEnd/api/controller/UserController.java
@@ -6,7 +6,6 @@ import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.validation.Errors;
 import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -15,7 +14,9 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.drdoc.BackEnd.api.domain.dto.BaseResponseDto;
 import com.drdoc.BackEnd.api.domain.dto.RefreshTokenDto;
@@ -24,32 +25,34 @@ import com.drdoc.BackEnd.api.domain.dto.UserLoginRequestDto;
 import com.drdoc.BackEnd.api.domain.dto.UserLoginResponseDto;
 import com.drdoc.BackEnd.api.domain.dto.UserLogoutRequestDto;
 import com.drdoc.BackEnd.api.domain.dto.UserRegisterRequestDto;
+import com.drdoc.BackEnd.api.service.S3Service;
 import com.drdoc.BackEnd.api.service.UserService;
 
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
-import lombok.RequiredArgsConstructor;
 import springfox.documentation.annotations.ApiIgnore;
 
 @Api(value = "유저 API", tags = { "User 관리" })
 @RestController
 @CrossOrigin("*")
 @RequestMapping("/api/user")
-@RequiredArgsConstructor
 public class UserController {
 
-	private final UserService userService;
 	@Autowired
-	PasswordEncoder passwordEncoder;
+	private S3Service s3Service;
+
+	@Autowired
+	private UserService userService;
 
 	@ApiOperation(value = "회원가입", notes = "유저 정보 삽입")
 	@PostMapping("/public/signup")
 	@ApiResponses({ @ApiResponse(code = 200, message = "성공"), @ApiResponse(code = 400, message = "부적절한 요청"),
 			@ApiResponse(code = 500, message = "서버 오류") })
-	public ResponseEntity<BaseResponseDto> register(@RequestBody @Valid UserRegisterRequestDto requestDto,
-			@ApiIgnore Errors errors) {
+	public ResponseEntity<BaseResponseDto> register(
+			@RequestPart(value = "user") @Valid UserRegisterRequestDto requestDto,
+			@RequestPart("file") MultipartFile file, @ApiIgnore Errors errors) {
 		if (errors.hasErrors()) {
 			List<ObjectError> errorMessages = errors.getAllErrors();
 
@@ -59,41 +62,59 @@ public class UserController {
 
 			return ResponseEntity.status(400).body(BaseResponseDto.of(400, "잘못된 요청입니다."));
 		}
+		try {
+			if (file != null) {
+				if (file.getSize() >= 10485760) {
+					return ResponseEntity.status(400).body(BaseResponseDto.of(400, "이미지 크기 제한은 10MB 입니다."));
+				}
+				String originFile = file.getOriginalFilename();
+				String originFileExtension = originFile.substring(originFile.lastIndexOf("."));
+				if (!originFileExtension.equalsIgnoreCase(".jpg") && !originFileExtension.equalsIgnoreCase(".png")
+						&& !originFileExtension.equalsIgnoreCase(".jpeg")) {
+					return ResponseEntity.status(400).body(BaseResponseDto.of(400, "jpg, jpeg, png의 이미지 파일만 업로드해주세요"));
+				}
+				String imgPath = s3Service.upload("", file);
+				requestDto.setProfile_pic(imgPath);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+			return ResponseEntity.status(400).body(BaseResponseDto.of(400, "파일 업로드에 실패했습니다."));
+		}
 		userService.register(requestDto);
-		return ResponseEntity.status(200).body(BaseResponseDto.of(200, "Success"));
+		return ResponseEntity.status(201).body(BaseResponseDto.of(201, "Created"));
 	}
-	
+
 	@ApiOperation(value = "아이디 중복체크", notes = "memberId 중복체크")
 	@GetMapping("/public/id/{id}")
-	@ApiResponses({ @ApiResponse(code = 200, message = "사용가능한 아이디입니다."), @ApiResponse(code = 400, message = "중복된 아이디입니다."),
-			@ApiResponse(code = 500, message = "서버 오류") })
+	@ApiResponses({ @ApiResponse(code = 200, message = "사용가능한 아이디입니다."),
+			@ApiResponse(code = 400, message = "중복된 아이디입니다."), @ApiResponse(code = 500, message = "서버 오류") })
 	public ResponseEntity<BaseResponseDto> checkMemberId(@PathVariable("id") String memberId) {
 		if (userService.checkMemberId(memberId)) {
 			return ResponseEntity.status(200).body(BaseResponseDto.of(200, "사용가능한 아이디입니다."));
 		}
 		return ResponseEntity.status(400).body(BaseResponseDto.of(400, "중복된 아이디입니다."));
 	}
-	
+
 	@ApiOperation(value = "닉네임 중복체크", notes = "nickname 중복체크")
 	@GetMapping("/public/nickname/{nickname}")
-	@ApiResponses({ @ApiResponse(code = 200, message = "사용가능한 닉네임입니다."), @ApiResponse(code = 400, message = "중복된 닉네임입니다."),
-			@ApiResponse(code = 500, message = "서버 오류") })
+	@ApiResponses({ @ApiResponse(code = 200, message = "사용가능한 닉네임입니다."),
+			@ApiResponse(code = 400, message = "중복된 닉네임입니다."), @ApiResponse(code = 500, message = "서버 오류") })
 	public ResponseEntity<BaseResponseDto> checkNickname(@PathVariable("nickname") String nickname) {
 		if (userService.checkNickname(nickname)) {
 			return ResponseEntity.status(200).body(BaseResponseDto.of(200, "사용가능한 닉네임입니다."));
 		}
 		return ResponseEntity.status(400).body(BaseResponseDto.of(400, "중복된 닉네임입니다."));
 	}
-	
+
 	@ApiOperation(value = "로그인", notes = "memberId와 password를 사용해 로그인")
 	@PostMapping("/public/login")
-	@ApiResponses({ @ApiResponse(code = 200, message = "로그인 성공"), @ApiResponse(code = 400, message = "회원정보가 일치하지 않습니다."),
-			@ApiResponse(code = 500, message = "서버 오류") })
+	@ApiResponses({ @ApiResponse(code = 200, message = "로그인 성공"),
+			@ApiResponse(code = 400, message = "회원정보가 일치하지 않습니다."), @ApiResponse(code = 500, message = "서버 오류") })
 	public ResponseEntity<BaseResponseDto> login(@RequestBody UserLoginRequestDto userLoginRequestDto) {
 		TokenDto tokenDto = userService.login(userLoginRequestDto);
 		return ResponseEntity.status(200).body(UserLoginResponseDto.of(200, "로그인 성공", tokenDto));
 	}
-	
+
 	@ApiOperation(value = "Access 토큰 재발급", notes = "JWT Refresh 토큰을 사용해 재발급")
 	@PostMapping("/auth/reissue")
 	@ApiResponses({ @ApiResponse(code = 200, message = "재발급 성공"), @ApiResponse(code = 400, message = "정보가 일치하지 않습니다."),
@@ -102,11 +123,11 @@ public class UserController {
 		TokenDto tokenDto = userService.reissue(tokenRequestDto);
 		return ResponseEntity.status(200).body(UserLoginResponseDto.of(200, "재발급 성공", tokenDto));
 	}
-	
+
 	@ApiOperation(value = "로그아웃", notes = "현재 사용중인 Refresh Token을 DB에서 삭제")
 	@PostMapping("/auth/logout")
-	@ApiResponses({ @ApiResponse(code = 200, message = "로그아웃 성공"), @ApiResponse(code = 400, message = "회원정보가 일치하지 않습니다."),
-			@ApiResponse(code = 500, message = "서버 오류") })
+	@ApiResponses({ @ApiResponse(code = 200, message = "로그아웃 성공"),
+			@ApiResponse(code = 400, message = "회원정보가 일치하지 않습니다."), @ApiResponse(code = 500, message = "서버 오류") })
 	public ResponseEntity<BaseResponseDto> logout(@RequestBody UserLogoutRequestDto userLogoutRequestDto) {
 		userService.logout(userLogoutRequestDto.getRefresh_token());
 		return ResponseEntity.status(200).body(BaseResponseDto.of(200, "로그아웃 성공"));
@@ -159,7 +180,6 @@ public class UserController {
 //            else return new ResponseEntity<String>("비밀번호 틀림", HttpStatus.FORBIDDEN);
 //        }
 //    }
-
 
 //    @PostMapping("/modify")
 //    @ApiOperation(value = "학생 정보 수정", notes = "학생정보 수정")
