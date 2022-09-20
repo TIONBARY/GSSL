aefee6851da3523e1baf0d5ed0d9858bbb32dedd
한준수 <junsoohan94@gmail.com>
2022-09-20 16:06:47 +0900

    ✨ 산책 기록 조회 기능 구현

--- BackEnd/src/main/java/com/drdoc/BackEnd/api/domain/dto/WalkDetailDto.java
+++ BackEnd/src/main/java/com/drdoc/BackEnd/api/domain/dto/WalkDetailDto.java
@@ -3,7 +3,6 @@ package com.drdoc.BackEnd.api.domain.dto;
 import java.time.LocalDateTime;
 import java.util.List;
 
-import com.drdoc.BackEnd.api.domain.Pet;
 import com.drdoc.BackEnd.api.domain.Walk;
 
 import lombok.AllArgsConstructor;
@@ -18,17 +17,27 @@ import lombok.Setter;
 @AllArgsConstructor
 @Builder
 public class WalkDetailDto {
-	
-    private LocalDateTime start_time;
-    private LocalDateTime end_time;
-    private int distance;
-    private List<Pet> walkPetsList;
-    
+
+	private int walk_id;
+	private LocalDateTime start_time;
+	private LocalDateTime end_time;
+	private int distance;
+	private List<WalkPetDetailDto> petsList;
+
 	public WalkDetailDto(Walk walk) {
+		this.walk_id = walk.getId();
 		this.start_time = walk.getStart_time();
 		this.end_time = walk.getEnd_time();
 		this.distance = walk.getDistance();
-//		this.walkPetsList = walk.getWalkPetId();
 	}
-    
+
+	public WalkDetailDto(Walk walk, List<WalkPetDetailDto> petsList) {
+		this(walk);
+		this.petsList = petsList;
+	}
+
+	public void editPetList(List<WalkPetDetailDto> petsList) {
+		this.petsList = petsList;
+	}
+
 }
