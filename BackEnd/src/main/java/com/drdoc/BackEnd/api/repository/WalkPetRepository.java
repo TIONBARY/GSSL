fb4543d07890a989b2e9d25d4ec14e9cdef7da88
한준수 <junsoohan94@gmail.com>
2022-09-20 13:48:57 +0900

    ✨ 산책 기록 삭제 기능 구현

--- BackEnd/src/main/java/com/drdoc/BackEnd/api/repository/WalkPetRepository.java
+++ BackEnd/src/main/java/com/drdoc/BackEnd/api/repository/WalkPetRepository.java
@@ -1,6 +1,7 @@
 package com.drdoc.BackEnd.api.repository;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -11,7 +12,7 @@ import com.drdoc.BackEnd.api.domain.WalkPet;
 
 @Repository
 public interface WalkPetRepository extends JpaRepository<WalkPet, Integer> {
-	WalkPet findFirstByWalkAndPet(Walk walk, Pet pet);
-	List<WalkPet> findByWalk(Walk walk);
+	Optional<WalkPet> findFirstByWalkAndPet(Walk walk, Pet pet);
+	Optional<List<WalkPet>> findByWalk(Walk walk);
 
 }
--- BackEnd/src/main/java/com/drdoc/BackEnd/api/service/WalkServiceImpl.java
+++ BackEnd/src/main/java/com/drdoc/BackEnd/api/service/WalkServiceImpl.java
@@ -3,6 +3,8 @@ package com.drdoc.BackEnd.api.service;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import javax.transaction.Transactional;
+
 import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Service;
 
@@ -27,6 +29,7 @@ public class WalkServiceImpl implements WalkService {
 	private final WalkPetRepository walkPetRepository;
 
 	// 산책 기록 등록
+	@Transactional
 	public void register(WalkRegisterRequestDto request) {
 		// 산책 테이블에 저장
 		Walk walk = new Walk(request);
@@ -41,18 +44,22 @@ public class WalkServiceImpl implements WalkService {
 	}
 
 	// 산책 기록 수정
+	@Transactional
 	public void modify(Integer walkId, WalkModifyRequestDto request) {
 		Walk walk = walkRepository.findById(walkId).get();
 		// 산책동물 테이블에 저장된 정보 수정
-		List<WalkPet> walkPetList = walkPetRepository.findByWalk(walk);
+		List<WalkPet> walkPetList = walkPetRepository.findByWalk(walk)
+				.orElseThrow(() -> new IllegalArgumentException("해당 산책에 데려간 반려동물이 없습니다."));
 		List<Integer> oldPetIds = walkPetList.stream().map(wp -> wp.getPet().getId()).collect(Collectors.toList());
 		List<Integer> newPetIds = request.getPet_ids();
-		
+
 		// 기존 펫 제거
 		for (int oldPetId : oldPetIds) {
 			if (!newPetIds.contains(oldPetId)) {
-				Pet pet = petRepository.findById(oldPetId).get();
-				WalkPet walkPet = walkPetRepository.findFirstByWalkAndPet(walk, pet);
+				Pet pet = petRepository.findById(oldPetId)
+						.orElseThrow(() -> new IllegalArgumentException("존재하지 않는 반려동물입니다."));
+				WalkPet walkPet = walkPetRepository.findFirstByWalkAndPet(walk, pet)
+						.orElseThrow(() -> new IllegalArgumentException("해당 반려동물을 산책에 데려가지 않았습니다."));
 				walkPetRepository.delete(walkPet);
 			} else {
 				newPetIds.remove((Integer) oldPetId);
@@ -61,18 +68,22 @@ public class WalkServiceImpl implements WalkService {
 
 		// 새 펫 추가
 		for (int newPetId : newPetIds) {
-			Pet pet = petRepository.findById(newPetId).get();
+			Pet pet = petRepository.findById(newPetId)
+					.orElseThrow(() -> new IllegalArgumentException("존재하지 않는 반려동물입니다."));
 			WalkPet walkPet = new WalkPet(walk, pet);
 			walkPetRepository.save(walkPet);
 		}
 	}
 
 	// 산책 기록 삭제
-	public void delete(int WalkId) {
+	public void delete(int walkId) {
+		walkRepository.findById(walkId).orElseThrow(() -> new IllegalArgumentException("산책 기록을 찾을 수 없습니다."));
+		walkRepository.deleteById(walkId);
 	}
 
 	// 산책 기록 일괄 삭제
-	public void batchDelete(WalkBatchDeleteRequestDto Walks) {
+	public void batchDelete(WalkBatchDeleteRequestDto walks) {
+		walkRepository.deleteAllByIdInBatch(walks.getWalk_ids());		
 	}
 
 	// 산책 기록 전체 조회
@@ -81,7 +92,7 @@ public class WalkServiceImpl implements WalkService {
 	}
 
 	// 산책 기록 상세 조회
-	public WalkDetailDto detail(int WalkId) {
+	public WalkDetailDto detail(int walkId) {
 		return null;
 	}
 }
