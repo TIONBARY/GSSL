def1de072357eeafedc2b74682ed431e90761f57
한준수 <junsoohan94@gmail.com>
2022-09-20 11:50:04 +0900

    ✨ 산책 등록 기능 구현

--- BackEnd/src/main/java/com/drdoc/BackEnd/api/repository/WalkPetRepository.java
+++ BackEnd/src/main/java/com/drdoc/BackEnd/api/repository/WalkPetRepository.java
@@ -3,9 +3,9 @@ package com.drdoc.BackEnd.api.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.drdoc.BackEnd.api.domain.Kind;
+import com.drdoc.BackEnd.api.domain.WalkPet;
 
 @Repository
-public interface WalkPetRepository extends JpaRepository<Kind, Integer>{
+public interface WalkPetRepository extends JpaRepository<WalkPet, Integer>{
 
 }
