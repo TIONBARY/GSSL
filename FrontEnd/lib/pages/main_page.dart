2e0fe022c01bc38f41db59914e104ccda6afb2f7
Dohoon Kim <97091891+Hooon97@users.noreply.github.com>
2022-09-21 17:38:37 +0900

    :twisted_rightwards_arrows: CSS 및 기능 작업 합병

--- FrontEnd/lib/pages/main_page.dart
+++ FrontEnd/lib/pages/main_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 import '../widget/main_function_box.dart';
 
 class MainPage extends StatefulWidget {
@@ -12,24 +13,66 @@ class _MainPageState extends State<MainPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title:Text("메인 페이지")),
-      body: Container(child: Column(
-        children: [
-          Flexible(child: UserBar(),flex: 1,),
-          Flexible(child: behavior_diagnosis(),flex: 2,),
-          Flexible(child: health_diagnosis(),flex: 2,),
-          Flexible(child: diary(),flex: 2,),
-        ],
-      ),),
-      bottomNavigationBar: BottomAppBar(
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      resizeToAvoidBottomInset: false,
+      appBar: AppBar(
+        toolbarHeight: 0,
+      ),
+      body: Container(
+        color: Color(0xFFFFFDF4),
+        child: Column(
           children: [
-            IconButton(icon: Icon(Icons.menu),onPressed: () {},),
-            IconButton(icon: Icon(Icons.search),onPressed: () {},),
-            IconButton(icon: Icon(Icons.star),onPressed: () {},),
-            IconButton(icon: Icon(Icons.more_vert),onPressed: () {},),
+            Flexible(
+              child: UserBar(),
+              flex: 1,
+            ),
+            Flexible(
+              child: behavior_diagnosis(),
+              flex: 2,
+            ),
+            Flexible(
+              child: health_diagnosis(),
+              flex: 2,
+            ),
+            Flexible(
+              child: diary(),
+              flex: 2,
+            ),
           ],
+        ),
+      ),
+      bottomNavigationBar: BottomAppBar(
+        color: Color(0xFFFFE6BC),
+        child: SizedBox(
+          height: 60,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              IconButton(
+                icon: Icon(
+                  Icons.people_alt_outlined,
+                  size: 30,
+                  color: Color(0xFFFFF3E4),
+                ),
+                onPressed: () {},
+              ),
+              IconButton(
+                icon: Icon(
+                  Icons.home,
+                  size: 30,
+                  color: Color(0xFF483434),
+                ),
+                onPressed: () {},
+              ),
+              IconButton(
+                icon: Icon(
+                  Icons.location_on_outlined,
+                  size: 30,
+                  color: Color(0xFFFFF3E4),
+                ),
+                onPressed: () {},
+              ),
+            ],
+          ),
         ),
       ),
     );
@@ -41,23 +84,76 @@ class UserBar extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(child: Row(
-      children: [Flexible(child:Container(color:Colors.blue)),
-        Flexible(child: Container(child: Column(
-          children: [
-            Flexible(child: Container(color:Colors.black, 
-              child: Container(child: Text("주인의 멍멍이", style: TextStyle(color: Colors.white),), width: double.infinity, height: double.infinity,) ,),flex: 1,),
-            Flexible(child: Container(color:Colors.red,
-              child: Container(child: Text("건강하자", style: TextStyle(color: Colors.white),), width: double.infinity, height: double.infinity,) ,),flex: 1,),
-          ],
-        ),
-        ),flex: 3,),
-        Flexible(child: Container(color:Colors.green,
-        child:
-        Container(child: Icon(Icons.phone), width: double.infinity, height: double.infinity,),),
-          flex: 1,),
-      ],
-    ),);
+    return Container(
+      margin: EdgeInsets.fromLTRB(0, 30, 0, 0),
+      // padding: EdgeInsets.fromLTRB(0, 10, 0, 10),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(45),
+        color: Color(0xFFFFE6BC),
+      ),
+      child: Row(
+        children: [
+          Flexible(
+              child: Container(
+                // color: Colors.blue
+              )),
+          Flexible(
+            child: Container(
+              child: Column(
+                // crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Flexible(
+                    child: Container(
+                      child: Container(
+                        padding: EdgeInsets.fromLTRB(0, 10, 0, 10),
+                        child: Text(
+                          "주인의 멍멍이",
+                          style: TextStyle(color: Color(0xFF483434)),
+                        ),
+                        width: double.infinity,
+                        height: double.infinity,
+                      ),
+                    ),
+                    flex: 1,
+                  ),
+                  Flexible(
+                    child: Container(
+                      child: Container(
+                        padding: EdgeInsets.fromLTRB(0, 10, 0, 10),
+                        child: Text(
+                          "건강하자",
+                          style: TextStyle(
+                            color: Color(0xFF483434),
+                          ),
+                        ),
+                        width: double.infinity,
+                        height: double.infinity,
+                      ),
+                    ),
+                    flex: 1,
+                  ),
+                ],
+              ),
+            ),
+            flex: 3,
+          ),
+          Flexible(
+            child: Container(
+              // color: Colors.green,
+              child: Container(
+                child: Icon(
+                  Icons.wifi_protected_setup_outlined,
+                  color: Color(0xFF483434),
+                ),
+                width: double.infinity,
+                height: double.infinity,
+              ),
+            ),
+            flex: 1,
+          ),
+        ],
+      ),
+    );
   }
 }
 
@@ -66,7 +162,11 @@ class behavior_diagnosis extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return function_box(title: '견민정음', box_color: Colors.blue, paddings: EdgeInsets.fromLTRB(30, 30, 30, 30),);
+    return function_box(
+      title: '견민정음',
+      box_color: Color(0x80DFB45B),
+      paddings: EdgeInsets.fromLTRB(30, 30, 30, 15),
+    );
   }
 }
 
@@ -75,7 +175,11 @@ class health_diagnosis extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return function_box(title: '견의보감', box_color: Colors.green, paddings: EdgeInsets.fromLTRB(30, 30, 30, 30),);
+    return function_box(
+      title: '견의보감',
+      box_color: Color(0x80506274),
+      paddings: EdgeInsets.fromLTRB(30, 15, 30, 15),
+    );
   }
 }
 
@@ -84,9 +188,10 @@ class diary extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return function_box(title: '견중일기', box_color: Colors.black, paddings: EdgeInsets.fromLTRB(30, 30, 30, 30),);
+    return function_box(
+      title: '견중일기',
+      box_color: Color(0x80C66952),
+      paddings: EdgeInsets.fromLTRB(30, 15, 30, 30),
+    );
   }
 }
-
-
-
--- FrontEnd/lib/widget/main_function_box.dart
+++ FrontEnd/lib/widget/main_function_box.dart
@@ -1,7 +1,12 @@
 import 'package:flutter/material.dart';
 
 class function_box extends StatelessWidget {
-  const function_box({Key? key, required this.title, required this.box_color, required this.paddings}) : super(key: key);
+  const function_box(
+      {Key? key,
+        required this.title,
+        required this.box_color,
+        required this.paddings})
+      : super(key: key);
   final title;
   final box_color;
   final paddings;
@@ -10,29 +15,59 @@ class function_box extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       padding: paddings,
-      child: Container(child: Column(
-        children: [
-          Flexible(child: Container(width:480, height:360, color:box_color,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Container(child:
-                Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
+      child: Container(
+        child: Column(
+          children: [
+            Flexible(
+              child: Container(
+                width: 480,
+                height: 360,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(15),
+                  color: box_color,
+                ),
+                child: Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                    Padding(
-                      padding: const EdgeInsets.all(15.0),
-                      child: Text("${title}",style: TextStyle(fontSize: 40, fontFamily: "Daehan"),),
+                    Container(
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.fromLTRB(15, 0, 0, 0),
+                            child: Text(
+                              "${title}",
+                              style: TextStyle(
+                                fontSize: 70,
+                                color: Color(0xFF483434),
+                                fontFamily: "Daehan",
+                              ),
+                            ),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.fromLTRB(15, 0, 0, 0),
+                            child: Text(
+                              "설명",
+                              style: TextStyle(
+                                color: Color(0xFF483434),
+                                fontFamily: "Daehan",
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
                     ),
                   ],
                 ),
-                  width: 200, height: 100, color: Colors.red,),
-              ],
-            ),),flex: 1,),
-        ],
-      ),),
-    );;;
+              ),
+              flex: 1,
+            ),
+          ],
+        ),
+      ),
+    );
+    ;
+    ;
   }
 }
