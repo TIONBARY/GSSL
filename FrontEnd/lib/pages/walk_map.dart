ebf774388b37150d1d7b9cbca69ce7f505388b81
한준수 <junsoohan94@gmail.com>
2022-09-23 14:51:06 +0900

    ✨ 이동한 경로 출력 기능 구현
    - GPS 위치 권한 허용 창 추가
    - Kakao Map polyline 이용해서 이동 경로 선으로 출력

--- FrontEnd/lib/pages/walk_map.dart
+++ FrontEnd/lib/pages/walk_map.dart
@@ -1,14 +1,17 @@
-import 'dart:convert';
 import 'dart:async';
+import 'dart:convert';
 
 import 'package:flutter/material.dart';
-import 'package:kakaomap_webview/kakaomap_webview.dart';
-import 'package:webview_flutter/webview_flutter.dart';
 import 'package:geolocator/geolocator.dart';
+import 'package:kakaomap_webview/kakaomap_webview.dart';
+import 'package:permission_handler/permission_handler.dart';
+import 'package:webview_flutter/webview_flutter.dart';
 
 const String kakaoMapKey = 'kakao자바스크립트API';
+List<Position> positionList = [];
+List<KakaoLatLng> kakaoPositionList = [];
 
-void main() {
+void main() async {
   runApp(MaterialApp(home: KakaoMapTest()));
 }
 
@@ -44,15 +47,108 @@ Future<Position> _determinePosition() async {
     }
   }
 
+  // 백그라운드 GPS 권한 요청
+  PermissionStatus alwaysPermission = await Permission.locationAlways.status;
+  if (alwaysPermission == PermissionStatus.granted) {
+    return await Geolocator.getCurrentPosition();
+  } else if (alwaysPermission == PermissionStatus.permanentlyDenied) {
+    return Future.error('위치정보 권한이 영구적으로 거부되어 권한을 요청할 수 없습니다.');
+  } else {
+    Permission.locationAlways.request();
+  }
+
   if (permission == LocationPermission.deniedForever) {
     // Permissions are denied forever, handle appropriately.
-    return Future.error(
-        '위치정보 권한이 영구적으로 거부되어 권한을 요청할 수 없습니다.');
+    return Future.error('위치정보 권한이 영구적으로 거부되어 권한을 요청할 수 없습니다.');
   }
 
   // When we reach here, permissions are granted and we can
   // continue accessing the position of the device.
   return await Geolocator.getCurrentPosition();
+}
+
+void startWalk(Position position, _mapController) {
+  // 연속적인 위치 정보 기록에 사용될 설정
+  final LocationSettings locationSettings = LocationSettings(
+    accuracy: LocationAccuracy.high,
+    distanceFilter: 100,
+  );
+
+  StreamSubscription<Position> positionStream =
+      Geolocator.getPositionStream(locationSettings: locationSettings)
+          .listen((Position? position) {
+    if (!positionList.contains(position)) {
+      if (positionList.length > 0) {
+        drawLine(_mapController, position, positionList.last);
+      }
+      positionList.add(position!);
+    }
+  });
+
+  var lat = position.latitude, // 위도
+      lon = position.longitude; // 경도
+  debugPrint('[버튼 클릭]');
+  debugPrint('$position');
+  debugPrint(lat.toString());
+  debugPrint(lon.toString());
+
+  if (positionList.length == 0) {
+    _mapController.runJavascript('''
+                  if ('$position') {
+                  // GeoLocator을 이용해서 얻어온 접속 위치로 이동합니다
+                    var lat = parseFloat('$lat'), // 위도
+                        lon = parseFloat('$lon'); // 경도
+                    var locPosition = new kakao.maps.LatLng(lat, lon);
+                    map.setCenter(locPosition);
+                      
+                  } else { // 위치정보를 사용할 수 없을때 이동할 위치를 설정합니다
+                  
+                    var locPosition = new kakao.maps.LatLng(37.5013068, 127.0396597); // 멀티캠퍼스 위치
+                    map.setCenter(locPosition);
+                  }
+            ''');
+  }
+}
+
+void drawLine(_mapController, position, beforePos) {
+  var lat = 0.0, lon = 0.0;
+  var beforeLat = 0.0, beforeLon = 0.0;
+
+  lat = position.latitude;
+  lon = position.longitude;
+  beforeLat = beforePos.latitude;
+  beforeLon = beforePos.longitude;
+  kakaoPositionList
+      .add(KakaoLatLng(lat: position.latitude, lng: position.longitude));
+  debugPrint('그리는 중');
+  _mapController.runJavascript('''
+                    var lat = parseFloat('$lat'), // 위도
+                        lon = parseFloat('$lon'); // 경도
+                    var beforeLat = parseFloat('$beforeLat'), // 위도
+                        beforeLon = parseFloat('$beforeLon'); // 경도
+  var locPosition = new kakao.maps.LatLng(lat, lon);
+  var beforeLocPosition = new kakao.maps.LatLng(beforeLat, beforeLon);
+  var linePath = [];
+  map.setCenter(locPosition);
+  linePath.push(beforeLocPosition);
+  linePath.push(locPosition);
+
+// 지도에 표시할 선을 생성합니다
+var polyline = new kakao.maps.Polyline({
+    path: linePath, // 선을 구성하는 좌표배열 입니다
+    strokeWeight: 5, // 선의 두께 입니다
+    strokeColor: '#FFAE00', // 선의 색깔입니다
+    strokeOpacity: 0.7, // 선의 불투명도 입니다 1에서 0 사이의 값이며 0에 가까울수록 투명합니다
+    strokeStyle: 'solid' // 선의 스타일입니다
+});
+
+// 지도에 선을 표시합니다 
+polyline.setMap(map);
+            ''');
+}
+
+void stopWalk() {
+  debugPrint('산책 끝');
 }
 
 class KakaoMapTest extends StatefulWidget {
@@ -65,11 +161,22 @@ class _KakaoMapTestState extends State<KakaoMapTest> {
   final double _lat = 37.5013068;
   final double _lng = 127.0396597;
 
-
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
     var appBarHeight = AppBar().preferredSize.height;
+    Position position = Position(
+        longitude: 127.0396597,
+        latitude: 37.5013068,
+        timestamp: DateTime(0),
+        accuracy: 0,
+        altitude: 0,
+        heading: 0,
+        speed: 0,
+        speedAccuracy: 0);
+
+    Future<Position> future = _determinePosition();
+    future.then((pos) => position = pos);
 
     return Scaffold(
       resizeToAvoidBottomInset: false,
@@ -80,12 +187,11 @@ class _KakaoMapTestState extends State<KakaoMapTest> {
           KakaoMapView(
             width: size.width,
             // height: size.height * 7 / 10,
-            height: size.height - appBarHeight - 80,
-            // height: size.height,
+            height: size.height - appBarHeight - 130,
             // height: size.height - 40,
             kakaoMapKey: kakaoMapKey,
-            lat: _lat,
-            lng: _lng,
+            lat: position.latitude,
+            lng: position.longitude,
             showMapTypeControl: false,
             showZoomControl: false,
             draggableMarker: false,
@@ -93,28 +199,18 @@ class _KakaoMapTestState extends State<KakaoMapTest> {
             mapController: (controller) {
               _mapController = controller;
             },
-            polyline: KakaoFigure(
-              path: [
-                KakaoLatLng(lat: 37.5013068, lng: 127.0396597),
-                KakaoLatLng(lat: 37.5113068, lng: 127.03917),
-                KakaoLatLng(lat: 37.5213068, lng: 127.03869),
-                KakaoLatLng(lat: 37.5313068, lng: 127.03971)
-              ],
-              strokeColor: Colors.blue,
-              strokeWeight: 2.5,
-              strokeColorOpacity: 0.9,
-            ),
+            polyline: KakaoFigure(path: []),
             zoomChanged: (message) {
               debugPrint('[확대] ${message.message}');
             },
             cameraIdle: (message) {
               KakaoLatLng latLng =
-              KakaoLatLng.fromJson(jsonDecode(message.message));
+                  KakaoLatLng.fromJson(jsonDecode(message.message));
               debugPrint('[대기중] ${latLng.lat}, ${latLng.lng}');
             },
             boundaryUpdate: (message) {
               KakaoBoundary boundary =
-              KakaoBoundary.fromJson(jsonDecode(message.message));
+                  KakaoBoundary.fromJson(jsonDecode(message.message));
               debugPrint(
                   '[범위] ne : ${boundary.neLat}, ${boundary.neLng}, sw : ${boundary.swLat}, ${boundary.swLng}');
             },
@@ -122,79 +218,16 @@ class _KakaoMapTestState extends State<KakaoMapTest> {
           ElevatedButton(
               child: Text('산책 시작'),
               onPressed: () async {
-                // await _openKakaoMapScreen(context);
                 // 1번 기록(최초 위치)
-                Position position = await Geolocator.getCurrentPosition(desiredAccuracy: LocationAccuracy.high);
-
-                // 연속적인 위치 정보 기록
-                final LocationSettings locationSettings = LocationSettings(
-                  accuracy: LocationAccuracy.high,
-                  distanceFilter: 100,
-                );
-                Position currentPosition;
-
-                StreamSubscription<Position> positionStream = Geolocator.getPositionStream(locationSettings: locationSettings).listen(
-                        (Position? position) {
-                      print(position == null ? 'Unknown' : '${position.latitude.toString()}, ${position.longitude.toString()}');
-                    });
-
-                var lat = position.latitude, // 위도
-                    lon = position.longitude; // 경도
-                debugPrint('[버튼 클릭]');
-                debugPrint('$position');
-                debugPrint(lat.toString());
-                debugPrint(lon.toString());
-
-                _mapController.runJavascript('''
-                  if ('$position') {
-                  // GeoLocation을 이용해서 접속 위치를 얻어옵니다
-                  
-                    // alert('$lat');
-                    // alert('$lon');
-                    var lat = parseFloat('$lat'), // 위도
-                        lon = parseFloat('$lon'); // 경도
-        
-                    // alert(lat);
-                    // alert(lon);
-                    var locPosition = new kakao.maps.LatLng(lat, lon), // 마커가 표시될 위치를 geolocation으로 얻어온 좌표로 생성합니다
-                        message = '<div style="padding:5px;">여기에 계신가요?!</div>'; // 인포윈도우에 표시될 내용입니다
-                    
-                    // 마커와 인포윈도우를 표시합니다
-                    displayMarker(locPosition, message);
-                      
-                  } else { // HTML5의 GeoLocation을 사용할 수 없을때 마커 표시 위치와 인포윈도우 내용을 설정합니다
-                      
-                      var locPosition = new kakao.maps.LatLng(33.450701, 126.570667),    
-                          message = 'geolocation을 사용할수 없어요..'
-                          
-                      displayMarker(locPosition, message);
-                  }
-                
-                // 지도에 마커와 인포윈도우를 표시하는 함수입니다
-                function displayMarker(locPosition, message) {
-                
-                    // 마커를 생성합니다
-                    var marker = new kakao.maps.Marker({  
-                        map: map, 
-                        position: locPosition
-                    }); 
-                    
-                    var iwContent = message, // 인포윈도우에 표시할 내용
-                        iwRemoveable = true;
-                
-                    // 인포윈도우를 생성합니다
-                    var infowindow = new kakao.maps.InfoWindow({
-                        content : iwContent,
-                        removable : iwRemoveable
-                    });
-                    
-                    // 인포윈도우를 마커위에 표시합니다 
-                    infowindow.open(map, marker);
-                    
-                    // 지도 중심좌표를 접속위치로 변경합니다
-                    map.setCenter(locPosition);      
-                }
-            ''');
+                Future<Position> future = _determinePosition();
+                future
+                    .then((pos) => startWalk(pos, _mapController))
+                    .catchError((error) => debugPrint(error));
+              }),
+          ElevatedButton(
+              child: Text('산책 종료'),
+              onPressed: () async {
+                stopWalk();
               })
         ],
       ),
