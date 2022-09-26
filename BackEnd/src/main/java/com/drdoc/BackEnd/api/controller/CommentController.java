92018f06052f7d95bd29ee778e62ebecd3ba56d9
윤지환 <dddalang4@naver.com>
2022-09-27 08:45:14 +0900

    :sparkles: 댓글 작성 기능 구현

---
+++ BackEnd/src/main/java/com/drdoc/BackEnd/api/controller/CommentController.java
@@ -0,0 +1,45 @@
+package com.drdoc.BackEnd.api.controller;
+
+import java.io.IOException;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.drdoc.BackEnd.api.domain.dto.BaseResponseDto;
+import com.drdoc.BackEnd.api.domain.dto.CommentWriteRequestDto;
+import com.drdoc.BackEnd.api.service.CommentService;
+import com.drdoc.BackEnd.api.util.SecurityUtil;
+
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+
+@Api(value = "커뮤니티 댓글 API", tags = { "Comment 관리" })
+@RestController
+@CrossOrigin("*")
+@RequestMapping("/api/comment")
+public class CommentController {
+
+	@Autowired
+	private CommentService commentService;
+
+	@PostMapping
+	@ApiOperation(value = "커뮤니티 댓글 등록", notes = "커뮤니티에 댓글을 등록합니다.")
+	@ApiResponses({ @ApiResponse(code = 201, message = "댓글 등록에 성공했습니다."),
+			@ApiResponse(code = 400, message = "입력이 잘못되었거나 입력 제한을 넘어갔습니다."),
+			@ApiResponse(code = 401, message = "인증이 만료되어 로그인이 필요합니다."), @ApiResponse(code = 500, message = "서버 오류") })
+	public ResponseEntity<BaseResponseDto> writeComment(
+			@Valid @RequestBody CommentWriteRequestDto requestDto) throws IOException {
+		String memberId = SecurityUtil.getCurrentUsername();
+		commentService.writeComment(memberId, requestDto);
+		return ResponseEntity.status(201).body(BaseResponseDto.of(201, "Created"));
+	}
+}
---
+++ BackEnd/src/main/java/com/drdoc/BackEnd/api/service/CommentServiceImpl.java
@@ -0,0 +1,60 @@
+package com.drdoc.BackEnd.api.service;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.drdoc.BackEnd.api.domain.Board;
+import com.drdoc.BackEnd.api.domain.Comment;
+import com.drdoc.BackEnd.api.domain.User;
+import com.drdoc.BackEnd.api.domain.dto.CommentListDto;
+import com.drdoc.BackEnd.api.domain.dto.CommentModifyRequestDto;
+import com.drdoc.BackEnd.api.domain.dto.CommentWriteRequestDto;
+import com.drdoc.BackEnd.api.repository.BoardRepository;
+import com.drdoc.BackEnd.api.repository.CommentRepository;
+import com.drdoc.BackEnd.api.repository.UserRepository;
+
+@Service
+public class CommentServiceImpl implements CommentService {
+	
+	@Autowired
+	private UserRepository userRepository;
+	
+	@Autowired
+	private BoardRepository boardRepository;
+	
+	@Autowired
+	private CommentRepository commentRepository;
+
+	@Override
+	public void writeComment(String memberId, CommentWriteRequestDto requestDto) {
+		User user = userRepository.findByMemberId(memberId)
+				.orElseThrow(() -> new IllegalArgumentException("가입하지 않은 계정입니다."));
+		Board board = boardRepository.findById(requestDto.getBoard_id())
+				.orElseThrow(() -> new IllegalArgumentException("존재하지 않는 게시글입니다."));
+		Comment comment = Comment.builder().user(user).board(board).
+				content(requestDto.getContent()).createdTime(LocalDateTime.now()).build();
+		commentRepository.save(comment);
+	}
+
+	@Override
+	public void modifyComment(int commentId, String memberId, CommentModifyRequestDto requestDto) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void deleteComment(int commentId, String memberId) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public List<CommentListDto> getCommentList(int boardId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
