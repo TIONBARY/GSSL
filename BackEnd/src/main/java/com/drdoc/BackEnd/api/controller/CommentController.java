3cdbac1b2190e702dca5043114a11e07404ab3fc
윤지환 <dddalang4@naver.com>
2022-09-27 08:59:01 +0900

    :sparkles: 댓글 삭제 기능 구현

--- BackEnd/src/main/java/com/drdoc/BackEnd/api/controller/CommentController.java
+++ BackEnd/src/main/java/com/drdoc/BackEnd/api/controller/CommentController.java
@@ -7,6 +7,7 @@ import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -38,7 +39,8 @@ public class CommentController {
 	@ApiOperation(value = "커뮤니티 댓글 등록", notes = "커뮤니티에 댓글을 등록합니다.")
 	@ApiResponses({ @ApiResponse(code = 201, message = "댓글 등록에 성공했습니다."),
 			@ApiResponse(code = 400, message = "입력이 잘못되었거나 입력 제한을 넘어갔습니다."),
-			@ApiResponse(code = 401, message = "인증이 만료되어 로그인이 필요합니다."), @ApiResponse(code = 500, message = "서버 오류") })
+			@ApiResponse(code = 401, message = "인증이 만료되어 로그인이 필요합니다."), 
+			@ApiResponse(code = 500, message = "서버 오류") })
 	public ResponseEntity<BaseResponseDto> writeComment(
 			@Valid @RequestBody CommentWriteRequestDto requestDto) throws IOException {
 		String memberId = SecurityUtil.getCurrentUsername();
@@ -50,11 +52,25 @@ public class CommentController {
 	@ApiOperation(value = "커뮤니티 댓글 수정", notes = "커뮤니티에 작성한 댓글을 수정합니다.")
 	@ApiResponses({ @ApiResponse(code = 201, message = "댓글 수정에 성공했습니다."),
 			@ApiResponse(code = 400, message = "입력이 잘못되었거나 입력 제한을 넘어갔습니다."),
-			@ApiResponse(code = 401, message = "인증이 만료되어 로그인이 필요합니다."), @ApiResponse(code = 500, message = "서버 오류") })
+			@ApiResponse(code = 401, message = "인증이 만료되어 로그인이 필요합니다."), 
+			@ApiResponse(code = 403, message = "댓글 수정 권한이 없습니다."), @ApiResponse(code = 500, message = "서버 오류") })
 	public ResponseEntity<BaseResponseDto> modifyComment(
 			@PathVariable("commentId") int commentId, @Valid @RequestBody CommentModifyRequestDto requestDto) throws IOException {
 		String memberId = SecurityUtil.getCurrentUsername();
 		commentService.modifyComment(commentId, memberId, requestDto);
 		return ResponseEntity.status(200).body(BaseResponseDto.of(200, "Modified"));
+	}
+	
+	@DeleteMapping("/{commentId}")
+	@ApiOperation(value = "커뮤니티 댓글 삭제", notes = "커뮤니티에 작성한 댓글을 삭제합니다.")
+	@ApiResponses({ @ApiResponse(code = 201, message = "댓글 삭제에 성공했습니다."),
+			@ApiResponse(code = 400, message = "입력이 잘못되었거나 입력 제한을 넘어갔습니다."),
+			@ApiResponse(code = 401, message = "인증이 만료되어 로그인이 필요합니다."), 
+			@ApiResponse(code = 403, message = "댓글 삭제 권한이 없습니다."), @ApiResponse(code = 500, message = "서버 오류") })
+	public ResponseEntity<BaseResponseDto> deleteComment(
+			@PathVariable("commentId") int commentId) throws IOException {
+		String memberId = SecurityUtil.getCurrentUsername();
+		commentService.deleteComment(commentId, memberId);
+		return ResponseEntity.status(200).body(BaseResponseDto.of(200, "Deleted"));
 	}
 }
