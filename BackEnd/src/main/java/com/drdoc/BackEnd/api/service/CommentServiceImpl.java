e9bc3f48914745eaea4a4f3423a4189397c52508
윤지환 <dddalang4@naver.com>
2022-09-27 08:59:01 +0900

    :sparkles: 댓글 삭제 기능 구현

--- BackEnd/src/main/java/com/drdoc/BackEnd/api/service/CommentServiceImpl.java
+++ BackEnd/src/main/java/com/drdoc/BackEnd/api/service/CommentServiceImpl.java
@@ -6,6 +6,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.drdoc.BackEnd.api.domain.Board;
 import com.drdoc.BackEnd.api.domain.Comment;
@@ -30,6 +31,7 @@ public class CommentServiceImpl implements CommentService {
 	private CommentRepository commentRepository;
 
 	@Override
+	@Transactional
 	public void writeComment(String memberId, CommentWriteRequestDto requestDto) {
 		User user = userRepository.findByMemberId(memberId)
 				.orElseThrow(() -> new IllegalArgumentException("가입하지 않은 계정입니다."));
@@ -41,6 +43,7 @@ public class CommentServiceImpl implements CommentService {
 	}
 
 	@Override
+	@Transactional
 	public void modifyComment(int commentId, String memberId, CommentModifyRequestDto requestDto) {
 		User user = userRepository.findByMemberId(memberId)
 				.orElseThrow(() -> new IllegalArgumentException("가입하지 않은 계정입니다."));
@@ -53,9 +56,15 @@ public class CommentServiceImpl implements CommentService {
 	}
 
 	@Override
+	@Transactional
 	public void deleteComment(int commentId, String memberId) {
-		// TODO Auto-generated method stub
-
+		User user = userRepository.findByMemberId(memberId)
+				.orElseThrow(() -> new IllegalArgumentException("가입하지 않은 계정입니다."));
+		Comment comment = commentRepository.findById(commentId)
+				.orElseThrow(() -> new IllegalArgumentException("존재하지 않는 게시글입니다."));
+		if (user.getId() != comment.getUser().getId())
+			throw new AccessDeniedException("권한이 없습니다.");
+		commentRepository.delete(comment);
 	}
 
 	@Override
