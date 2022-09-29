a2c35fa0c3728bbebf796f9bbbf9ffd7e0978bdb
chyuje96 <74063824+chyuje96@users.noreply.github.com>
2022-09-29 11:41:02 +0900

    :lipstick: 견중일기(일지) 페이지 수정 중

--- FrontEnd/lib/pages/main_page.dart
+++ FrontEnd/lib/pages/main_page.dart
@@ -35,7 +35,7 @@ class MyApp extends StatelessWidget {
       // theme: ThemeData(
       //   primarySwatch: Colors.blue,
       // ),
-      home: const MainPage(),
+      home: BottomNavBar(),
     );
   }
 }
@@ -57,7 +57,7 @@ class _MainPageState extends State<MainPage> {
         toolbarHeight: 0,
       ),
       body: Container(
-        color: Color(0xFFFFFDF4),
+        color: nWColor,
         child: Column(
           children: [
             Flexible(
