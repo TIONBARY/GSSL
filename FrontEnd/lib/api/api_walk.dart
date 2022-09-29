a8122af8cc6f417de04796b90f82354a527e9be7
한준수 <junsoohan94@gmail.com>
2022-09-29 10:37:24 +0900

    ♻ rest 통신에 interceptor.dart 사용
    - ➕ http_interceptor 의존성 추가
    - JWT 토큰 갱신 추가
    - 기존 api_walk를 인터셉터 사용하도록 변경

--- FrontEnd/lib/api/api_walk.dart
+++ FrontEnd/lib/api/api_walk.dart
@@ -1,32 +1,18 @@
 import 'dart:convert';
 
-import 'package:GSSL/api/use_JWT_Token.dart';
+import 'package:GSSL/api/interceptor.dart';
 import 'package:GSSL/model/request_models/put_walk.dart';
 import 'package:GSSL/model/response_models/general_response.dart';
-import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'package:http_interceptor/http/http.dart';
 
 class ApiWalk {
+  final client = InterceptedClient.build(interceptors: [
+    ApiInterceptor()
+  ]);
   Future<generalResponse> enterWalk(putWalk requestModel) async {
-    final storage = FlutterSecureStorage();
-    // read value
-    String? accessToken = await storage.read(key: 'Authorization');
-    // String? refreshToken = await storage.read(key: 'RefreshToken');
     String url = "https://j7a204.p.ssafy.io/api/walk";
-    final response = await apiUseToken()
-        .useJWTHeader(url, jsonEncode(requestModel.toJson()));
-
-    // debugPrint("accessToken");
-    // debugPrint(accessToken);
-    // final response = await http.post(Uri.parse(url),
-    //     headers: <String, String>{
-    //       'Content-Type': 'application/json; charset=UTF-8',
-    //       'Authorization': 'Bearer ' + accessToken!,
-    //     },
-    //     body: jsonEncode(requestModel.toJson()));
-
-    generalResponse walkPutResponse =
-        generalResponse.fromJson(json.decode(response.body));
-
+    final response = await client.post(Uri.parse(url), body: jsonEncode(requestModel.toJson()));
+    final walkPutResponse = generalResponse.fromJson(json.decode(response.body));
     return walkPutResponse;
   }
 }
