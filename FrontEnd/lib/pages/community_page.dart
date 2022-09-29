afed7930ab4c228006748c7cd11d83d5b082bda2
JunWoo <junwoo0127@naver.com>
2022-09-29 14:46:03 +0900

    :recycle: Community UI 변경

--- FrontEnd/lib/pages/community_page.dart
+++ FrontEnd/lib/pages/community_page.dart
@@ -1,12 +1,11 @@
-import 'package:flutter/material.dart';
 import 'package:GSSL/components/community/first_form.dart';
 import 'package:GSSL/components/community/second_form.dart';
 import 'package:GSSL/components/community/third_form.dart';
+import 'package:GSSL/constants.dart';
+import 'package:flutter/material.dart';
 
-void main() => runApp(MaterialApp(
-    debugShowCheckedModeBanner: false,
-    home: CommunityApp()
-));
+void main() => runApp(
+    MaterialApp(debugShowCheckedModeBanner: false, home: CommunityApp()));
 
 class CommunityApp extends StatelessWidget {
   const CommunityApp({Key? key}) : super(key: key);
@@ -14,27 +13,45 @@ class CommunityApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return DefaultTabController(
-      length: 3,
-      child: Scaffold(
-        appBar: AppBar(
-          title: Text('커뮤니티'),
-          centerTitle: true,
-          bottom: TabBar(
-            tabs: [
-              Tab(text: '자랑하기', icon: Icon(Icons.favorite)),
-              Tab(text: '진단공유', icon: Icon(Icons.share)),
-              Tab(text: '질문하기', icon: Icon(Icons.question_mark)),
+        length: 3,
+        child: Scaffold(
+          appBar: AppBar(
+            iconTheme: IconThemeData(
+              color: btnColor,
+              size: 20,
+            ),
+            toolbarHeight: 25,
+            backgroundColor: pColor,
+            title: Text(
+              '커뮤니티',
+              style: TextStyle(
+                color: Colors.black,
+                fontSize: 25,
+              ),
+            ),
+            centerTitle: true,
+            bottom: TabBar(
+              tabs: [
+                Tab(text: '자랑하기', icon: Icon(Icons.favorite)),
+                Tab(text: '진단공유', icon: Icon(Icons.share)),
+                Tab(text: '질문하기', icon: Icon(Icons.question_mark)),
+              ],
+              indicatorColor: btnColor,
+              unselectedLabelColor: sColor,
+              labelColor: btnColor,
+              labelStyle: TextStyle(
+                fontSize: 15,
+                fontFamily: "Daehan",
+              ),
+            ),
+          ),
+          body: TabBarView(
+            children: [
+              FirstPage(),
+              SecondPage(),
+              ThirdPage(),
             ],
           ),
-        ),
-        body: TabBarView(
-          children: [
-            FirstPage(),
-            SecondPage(),
-            ThirdPage(),
-          ],
-        ),
-      )
-    );
+        ));
   }
 }
