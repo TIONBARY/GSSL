bb133ccea88325e09ae904c1d9e066ce818efd37
윤지환 <dddalang4@naver.com>
2022-09-29 16:01:00 +0900

    :recycle: User, Pet API에 interceptor 적용
    * interceptor를 활용해 API 요청에 accessToken 자동 추가 및 accessToken 만료 시 refreshToken을 통해 reissue할 수 있도록 변경

--- FrontEnd/lib/api/api_pet.dart
+++ FrontEnd/lib/api/api_pet.dart
@@ -1,3 +1,4 @@
+import 'package:GSSL/api/interceptor.dart';
 import 'package:GSSL/model/request_models/pet_info.dart';
 import 'package:GSSL/model/response_models/general_response.dart';
 import 'package:GSSL/model/response_models/get_all_pet.dart';
@@ -5,6 +6,7 @@ import 'package:GSSL/model/response_models/get_all_pet_kind.dart';
 import 'package:GSSL/model/response_models/get_pet_detail.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:http/http.dart' as http;
+import 'package:http_interceptor/http/http.dart';
 import 'package:image_picker/image_picker.dart';
 import 'dart:convert';
 import 'package:http_parser/http_parser.dart';
@@ -12,18 +14,17 @@ import 'package:http_parser/http_parser.dart';
 
 class ApiPet {
 
+  final client = InterceptedClient.build(interceptors: [
+    ApiInterceptor()
+  ]);
+
   String api_url = "https://j7a204.p.ssafy.io/api/pet";
   final storage = new FlutterSecureStorage();
 
   Future<getAllPetKind> getPetKind() async {
     String url = api_url + "/kind";
-    String? accessToken = await storage.read(key: "Authorization");
-    Map<String, String> headers = { "Authorization": "Bearer "+accessToken!};
-    final response = await http.get(Uri.parse(url), headers: headers);
+    final response = await client.get(Uri.parse(url));
     getAllPetKind result = getAllPetKind.fromJson(json.decode(response.body));
-    print(result.statusCode);
-    print(result.message);
-    print(json.decode(response.body));
     return result;
   }
 
@@ -56,17 +57,14 @@ class ApiPet {
   }
 
   Future<getPetDetail> getPetDetailApi(int? petId) async {
-    String? accessToken = await storage.read(key: "Authorization");
-    Map<String, String> headers = { "Authorization": "Bearer "+accessToken!};
-    final response = await http.get(Uri.parse(api_url + "/" + petId.toString()), headers: headers);
+    String url = api_url + "/" + petId.toString();
+    final response = await client.get(Uri.parse(url));
     getPetDetail result = getPetDetail.fromJson(json.decode(response.body));
     return result;
   }
 
   Future<getAllPet> getAllPetApi() async {
-    String? accessToken = await storage.read(key: "Authorization");
-    Map<String, String> headers = { "Authorization": "Bearer "+accessToken!};
-    final response = await http.get(Uri.parse(api_url), headers: headers);
+    final response = await client.get(Uri.parse(api_url));
     getAllPet result = getAllPet.fromJson(json.decode(response.body));
     return result;
   }
