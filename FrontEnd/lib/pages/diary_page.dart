05ca8255327b415bff333a1fcbfbea481e128bb3
Dohoon Kim <97091891+Hooon97@users.noreply.github.com>
2022-09-30 12:55:51 +0900

    :sparkles: 견중일기 반응형으로 수정

--- FrontEnd/lib/pages/diary_page.dart
+++ FrontEnd/lib/pages/diary_page.dart
@@ -1,8 +1,8 @@
-import 'package:GSSL/constants.dart';
-import 'package:flutter/material.dart';
-
 import 'package:GSSL/components/diary/diary_form.dart';
 import 'package:GSSL/components/diary/walk_form.dart';
+import 'package:GSSL/constants.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class GalleryApp extends StatelessWidget {
   @override
@@ -14,24 +14,23 @@ class GalleryApp extends StatelessWidget {
         appBar: AppBar(
           iconTheme: IconThemeData(
             color: btnColor,
-            size: 20,
+            size: 15.h,
           ),
-          toolbarHeight: 25,
+          toolbarHeight: 20.h,
           backgroundColor: pColor,
           bottom: TabBar(
             tabs: [
-              Tab(text: '일지',  icon: Icon(Icons.menu_book)),
+              Tab(text: '일지', icon: Icon(Icons.menu_book)),
               Tab(text: '산책', icon: Icon(Icons.pets)),
             ],
             indicatorColor: btnColor,
             unselectedLabelColor: sColor,
             labelColor: btnColor,
             labelStyle: TextStyle(
-              fontSize: 15,
+              fontSize: 14.sp,
               fontFamily: "Daehan",
             ),
           ),
-
         ),
         body: TabBarView(
           children: [
