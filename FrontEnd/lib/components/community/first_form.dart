0b96faeff9b749251becb9c91825dbadab10bf7a
윤지환 <dddalang4@naver.com>
2022-09-30 16:12:41 +0900

    :sparkles: 커뮤니티 게시글 상세 조회 페이지 구현
    * 댓글 조회는 미구현

--- FrontEnd/lib/components/community/first_form.dart
+++ FrontEnd/lib/components/community/first_form.dart
@@ -1,8 +1,12 @@
 import 'dart:async';
 
 import 'package:GSSL/api/api_community.dart';
+import 'package:GSSL/api/api_user.dart';
+import 'package:GSSL/components/bottomNavBar.dart';
+import 'package:GSSL/components/community/board_detail_page.dart';
 import 'package:GSSL/components/util/custom_dialog.dart';
 import 'package:GSSL/model/response_models/get_board_list.dart';
+import 'package:GSSL/model/response_models/user_info.dart';
 import 'package:GSSL/pages/login_page.dart';
 import 'package:flutter/material.dart';
 
@@ -28,10 +32,37 @@ class _FirstPageState extends State<FirstPage> with TickerProviderStateMixin {
   TextEditingController searchController = TextEditingController();
   late Size _size;
   List<Content> _aidList = [];
+  User? user;
 
   ApiCommunity apiCommunity = ApiCommunity();
+  ApiUser apiUser = ApiUser();
 
   Timer? _debounce;
+
+  Future<void> getUser() async {
+    userInfo? userInfoResponse = await apiUser.getUserInfo();
+    if (userInfoResponse.statusCode == 200) {
+      setState(() {
+        user = userInfoResponse.user;
+      });
+    } else if (userInfoResponse.statusCode == 401) {
+      showDialog(
+          context: context,
+          builder: (BuildContext context) {
+            return CustomDialog("로그인이 필요합니다.", (context) => LoginScreen());
+          });
+    } else {
+      showDialog(
+          context: context,
+          builder: (BuildContext context) {
+            return CustomDialog(
+                userInfoResponse.message == null
+                    ? "알 수 없는 오류가 발생했습니다."
+                    : userInfoResponse.message!,
+                (context) => BottomNavBar());
+          });
+    }
+  }
 
   Future<bool> _getSearchList(String searchText) async {
     // final data = await dbHelper.getSearchList(searchText);
@@ -74,6 +105,7 @@ class _FirstPageState extends State<FirstPage> with TickerProviderStateMixin {
   void initState() {
     super.initState();
     _getList(0, 30);
+    getUser();
   }
 
   @override
@@ -163,107 +195,124 @@ class _FirstPageState extends State<FirstPage> with TickerProviderStateMixin {
                       shrinkWrap: true,
                       itemCount: _aidList.length,
                       itemBuilder: (context, index) {
-                        return Dismissible(
-                          background: DismissibleBackgroundWidget(
-                              alignment: Alignment.centerRight,
-                              icon: Icons.edit,
-                              backgroundColor: Theme.of(context).primaryColor),
-                          secondaryBackground: DismissibleBackgroundWidget(
-                            alignment: Alignment.centerLeft,
-                            icon: Icons.delete_outline_sharp,
-                            backgroundColor: Colors.red,
-                            iconColor: Colors.white,
-                          ),
-                          confirmDismiss: (DismissDirection direction) async {
-                            if (direction == DismissDirection.startToEnd) {
-                              return await showDialog(
-                                context: context,
-                                builder: (BuildContext context) {
-                                  return AlertDialog(
-                                    title: const Text("Confirm"),
-                                    content: const Text(
-                                        "Are you sure you wish to edit this item?"),
-                                    actions: <Widget>[
-                                      TextBtnWidget(
-                                        name: ' Edit ',
-                                        isStretch: false,
-                                        onTap: () {
-                                          context
-                                              .to(EditPostPage(
-                                                  _aidList[index].id!))
-                                              .then((value) {
-                                            if (value != null) {
-                                              if (value == true) {
-                                                setState(() {});
-                                                _getList(0, 30);
-                                              }
-                                            }
-                                            return context.back(false);
-                                          });
-                                        },
-                                      ),
-                                      TextBtnWidget(
-                                        name: 'Cancel',
-                                        btnColor: Colors.white,
-                                        onTap: () => context.back(false),
-                                        isStretch: false,
-                                      ),
-                                    ],
+                        return GestureDetector(
+                            onTap: () {
+                              Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                      builder: (context) => BoardDetailPage(
+                                          _aidList[index].id!)));
+                            },
+                            child: Dismissible(
+                              background: DismissibleBackgroundWidget(
+                                  alignment: Alignment.centerRight,
+                                  icon: Icons.edit,
+                                  backgroundColor:
+                                      Theme.of(context).primaryColor),
+                              secondaryBackground: DismissibleBackgroundWidget(
+                                alignment: Alignment.centerLeft,
+                                icon: Icons.delete_outline_sharp,
+                                backgroundColor: Colors.red,
+                                iconColor: Colors.white,
+                              ),
+                              confirmDismiss:
+                                  (DismissDirection direction) async {
+                                if (direction == DismissDirection.startToEnd &&
+                                    user?.nickname != null &&
+                                    user!.nickname ==
+                                        _aidList[index].nickname) {
+                                  return await showDialog(
+                                    context: context,
+                                    builder: (BuildContext context) {
+                                      return AlertDialog(
+                                        title: const Text("수정"),
+                                        content:
+                                            const Text("정말 해당 게시물을 수정하시겠습니까?"),
+                                        actions: <Widget>[
+                                          TextBtnWidget(
+                                            name: ' 수정 ',
+                                            isStretch: false,
+                                            onTap: () {
+                                              context
+                                                  .to(EditPostPage(
+                                                      _aidList[index].id!))
+                                                  .then((value) {
+                                                if (value != null) {
+                                                  if (value == true) {
+                                                    setState(() {});
+                                                    _getList(0, 30);
+                                                  }
+                                                }
+                                                return context.back(false);
+                                              });
+                                            },
+                                          ),
+                                          TextBtnWidget(
+                                            name: '취소',
+                                            btnColor: Colors.white,
+                                            onTap: () => context.back(false),
+                                            isStretch: false,
+                                          ),
+                                        ],
+                                      );
+                                    },
                                   );
-                                },
-                              );
-                            } else if (direction ==
-                                DismissDirection.endToStart) {
-                              return await showDialog(
-                                context: context,
-                                builder: (BuildContext context) {
-                                  return AlertDialog(
-                                    title: const Text("Confirm"),
-                                    content: const Text(
-                                        "Are you sure you wish to delete this item?"),
-                                    actions: <Widget>[
-                                      TextBtnWidget(
-                                        name: 'Delete',
-                                        nameColor: Colors.white,
-                                        btnColor: Colors.red,
-                                        onTap: () {
-                                          DatabaseServices()
-                                              .deleteItem(_aidList[index].id!,
-                                                  tableContent)
-                                              .then((value) {
-                                            if (value != null) {
-                                              context.back(true);
-                                            }
-                                          });
-                                        },
-                                        isStretch: false,
-                                      ),
-                                      TextBtnWidget(
-                                        name: 'Cancel',
-                                        btnColor: Colors.white,
-                                        onTap: () => context.back(false),
-                                        isStretch: false,
-                                      ),
-                                    ],
+                                } else if (direction ==
+                                        DismissDirection.endToStart &&
+                                    user?.nickname != null &&
+                                    user!.nickname ==
+                                        _aidList[index].nickname) {
+                                  return await showDialog(
+                                    context: context,
+                                    builder: (BuildContext context) {
+                                      return AlertDialog(
+                                        title: const Text("삭제"),
+                                        content:
+                                            const Text("정말 해당 게시물을 삭제하시겠습니까?"),
+                                        actions: <Widget>[
+                                          TextBtnWidget(
+                                            name: '삭제',
+                                            nameColor: Colors.white,
+                                            btnColor: Colors.red,
+                                            onTap: () {
+                                              DatabaseServices()
+                                                  .deleteItem(
+                                                      _aidList[index].id!,
+                                                      tableContent)
+                                                  .then((value) {
+                                                if (value != null) {
+                                                  context.back(true);
+                                                }
+                                              });
+                                            },
+                                            isStretch: false,
+                                          ),
+                                          TextBtnWidget(
+                                            name: '취소',
+                                            btnColor: Colors.white,
+                                            onTap: () => context.back(false),
+                                            isStretch: false,
+                                          ),
+                                        ],
+                                      );
+                                    },
                                   );
-                                },
-                              );
-                            }
-                            return null;
-                          },
-                          onDismissed: (direction) {
-                            if (direction == DismissDirection.startToEnd) {
-                            } else if (direction ==
-                                DismissDirection.endToStart) {
-                              _getList(0, 30);
-                            }
-                          },
-                          key: Key(_aidList[index].id!.toString()),
-                          child: ContentItemWidget(
-                              name: _aidList[index].title!,
-                              // body: _aidList[index].,
-                              photo: _aidList[index].image),
-                        );
+                                }
+                                return null;
+                              },
+                              onDismissed: (direction) {
+                                if (direction == DismissDirection.startToEnd) {
+                                } else if (direction ==
+                                    DismissDirection.endToStart) {
+                                  _getList(0, 30);
+                                }
+                              },
+                              key: Key(_aidList[index].id!.toString()),
+                              child: ContentItemWidget(
+                                  name: _aidList[index].title!,
+                                  // body: _aidList[index].,
+                                  photo: _aidList[index].image),
+                            ));
                       }),
                 ],
               ),
