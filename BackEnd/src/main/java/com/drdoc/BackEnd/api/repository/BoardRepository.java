6c35fda8a2764b3f580099d7faea7a9059bc0430
윤지환 <dddalang4@naver.com>
2022-10-02 23:12:08 +0900

    :wrench: 게시글 검색 기능을 위한 API 관련 코드 일부 수정
    * 제목을 이용해 검색 가능하도록 코드 수정
    * 게시글 리스트 조회 시 게시글을 작성한 사용자의 이미지 주소도 함께 첨부

--- BackEnd/src/main/java/com/drdoc/BackEnd/api/repository/BoardRepository.java
+++ BackEnd/src/main/java/com/drdoc/BackEnd/api/repository/BoardRepository.java
@@ -7,5 +7,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.drdoc.BackEnd.api.domain.Board;
 
 public interface BoardRepository extends JpaRepository<Board, Integer> {
-	Page<Board> findByTypeId(int typeId, Pageable pageable);
+	Page<Board> findByTypeIdAndTitleContains(int typeId, String title, Pageable pageable);
 }
