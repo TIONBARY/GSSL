c9502efdfb88f894725a81918f14f5d009b5f376
윤지환 <dddalang4@naver.com>
2022-10-04 13:27:34 +0900

    :sparkles: 일지 추가, 전체 조회, 상세 조회 기능 추가
    * 일지 추가는 견의보감 진단 완료 시 해당 내용을 바탕으로 추가 가능

--- FrontEnd/lib/model/response_models/detail_pet_journal.dart
+++ FrontEnd/lib/model/response_models/detail_pet_journal.dart
@@ -1,136 +1,46 @@
 import 'package:GSSL/model/response_models/general_response.dart';
 
-class detailPetJournal extends generalResponse{
-  JournalList? journalList;
+class detailPetJournal extends generalResponse {
+  JournalDetail? detail;
 
-  detailPetJournal(int statusCode, String message, JournalList journalList) : super(statusCode,message){
-    this.journalList = journalList;
+  detailPetJournal(int statusCode, String message, JournalDetail detail)
+      : super(statusCode, message) {
+    this.detail = detail;
   }
 
-  detailPetJournal.fromJson(Map<String, dynamic> json) : super(json['statusCode'],json['message']) {
-    journalList = json['journalList'] != null
-        ? new JournalList.fromJson(json['journalList'])
+  detailPetJournal.fromJson(Map<String, dynamic> json)
+      : super(json['statusCode'], json['message']) {
+    detail = json['detail'] != null
+        ? new JournalDetail.fromJson(json['detail'])
         : null;
   }
 }
 
-class JournalList {
-  List<Content>? content;
-  String? pageable;
-  bool? last;
-  int? totalElements;
-  int? totalPages;
-  int? number;
-  int? size;
-  Sort? sort;
-  int? numberOfElements;
-  bool? first;
-  bool? empty;
-
-  JournalList(
-      {this.content,
-        this.pageable,
-        this.last,
-        this.totalElements,
-        this.totalPages,
-        this.number,
-        this.size,
-        this.sort,
-        this.numberOfElements,
-        this.first,
-        this.empty});
-
-  JournalList.fromJson(Map<String, dynamic> json) {
-    if (json['content'] != null) {
-      content = <Content>[];
-      json['content'].forEach((v) {
-        content!.add(new Content.fromJson(v));
-      });
-    }
-    pageable = json['pageable'];
-    last = json['last'];
-    totalElements = json['totalElements'];
-    totalPages = json['totalPages'];
-    number = json['number'];
-    size = json['size'];
-    sort = json['sort'] != null ? new Sort.fromJson(json['sort']) : null;
-    numberOfElements = json['numberOfElements'];
-    first = json['first'];
-    empty = json['empty'];
-  }
-
-  Map<String, dynamic> toJson() {
-    final Map<String, dynamic> data = new Map<String, dynamic>();
-    if (this.content != null) {
-      data['content'] = this.content!.map((v) => v.toJson()).toList();
-    }
-    data['pageable'] = this.pageable;
-    data['last'] = this.last;
-    data['totalElements'] = this.totalElements;
-    data['totalPages'] = this.totalPages;
-    data['number'] = this.number;
-    data['size'] = this.size;
-    if (this.sort != null) {
-      data['sort'] = this.sort!.toJson();
-    }
-    data['numberOfElements'] = this.numberOfElements;
-    data['first'] = this.first;
-    data['empty'] = this.empty;
-    return data;
-  }
-}
-
-class Content {
-  int? journalId;
+class JournalDetail {
+  int? id;
   int? petId;
   String? picture;
+  String? part;
+  String? symptom;
   String? result;
   String? createdDate;
 
-  Content(
-      {this.journalId,
-        this.petId,
-        this.picture,
-        this.result,
-        this.createdDate});
+  JournalDetail(
+      {this.id,
+      this.petId,
+      this.picture,
+      this.part,
+      this.symptom,
+      this.result,
+      this.createdDate});
 
-  Content.fromJson(Map<String, dynamic> json) {
-    journalId = json['journal_id'];
+  JournalDetail.fromJson(Map<String, dynamic> json) {
+    id = json['id'];
     petId = json['pet_id'];
     picture = json['picture'];
+    part = json['part'];
+    symptom = json['symptom'];
     result = json['result'];
     createdDate = json['created_date'];
-  }
-
-  Map<String, dynamic> toJson() {
-    final Map<String, dynamic> data = new Map<String, dynamic>();
-    data['journal_id'] = this.journalId;
-    data['pet_id'] = this.petId;
-    data['picture'] = this.picture;
-    data['result'] = this.result;
-    data['created_date'] = this.createdDate;
-    return data;
-  }
-}
-
-class Sort {
-  bool? empty;
-  bool? sorted;
-  bool? unsorted;
-
-  Sort({this.empty, this.sorted, this.unsorted});
-
-  Sort.fromJson(Map<String, dynamic> json) {
-    empty = json['empty'];
-    sorted = json['sorted'];
-    unsorted = json['unsorted'];
-  }
-
-  Map<String, dynamic> toJson() {
-    final Map<String, dynamic> data = new Map<String, dynamic>();
-    data['empty'] = this.empty;
-    data['sorted'] = this.sorted;
-    data['unsorted'] = this.unsorted;
-    return data;
   }
 }
