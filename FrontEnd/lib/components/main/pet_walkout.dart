b862d58f789b298cb29231f3397c7b2c4ecba286
윤지환 <dddalang4@naver.com>
2022-10-05 15:36:47 +0900

    :bug: 메인 반려동물 변경 시 관련 위젯 갱신이 안 되는 오류 수정

--- FrontEnd/lib/components/main/pet_walkout.dart
+++ FrontEnd/lib/components/main/pet_walkout.dart
@@ -1,90 +1,36 @@
-import 'package:GSSL/api/api_walk.dart';
 import 'package:GSSL/constants.dart';
 import 'package:GSSL/model/response_models/get_walk_total.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
-import '../../api/api_pet.dart';
-import '../../api/api_user.dart';
 import '../../model/response_models/get_pet_detail.dart';
-import '../../model/response_models/get_walk_done.dart';
 import '../../model/response_models/user_info.dart';
 
-class pet_walkout extends StatefulWidget {
-  const pet_walkout({Key? key}) : super(key: key);
+class PetWorkout extends StatelessWidget {
+  PetWorkout({
+    Key? key,
+    this.mainPet,
+    this.walkInfo,
+    this.done,
+    this.user,
+    this.loadingPet,
+    this.loadingInfo,
+    this.loadingDone,
+  }) : super(key: key);
 
-  @override
-  State<pet_walkout> createState() => _pet_walkoutState();
-}
-
-class _pet_walkoutState extends State<pet_walkout> {
   Pet? mainPet;
   TotalInfo? walkInfo;
-  bool done = false;
+  bool? done;
   User? user;
 
-  ApiUser apiUser = ApiUser();
-  ApiPet apiPet = ApiPet();
-  ApiWalk apiWalk = ApiWalk();
-
-  bool _loadingPet = true;
-  bool _loadingInfo = true;
-  bool _loadingDone = true;
-
-  Future<void> getUser() async {
-    userInfo? userInfoResponse = await apiUser.getUserInfo();
-    if (userInfoResponse.statusCode == 200) {
-      setState(() {
-        user = userInfoResponse.user;
-      });
-      getMainPet();
-      getTotalInfo();
-      getIsDone();
-    }
-  }
-
-  Future<void> getMainPet() async {
-    getPetDetail? getMainPetResponse =
-        await apiPet.getPetDetailApi(user?.petId);
-    if (getMainPetResponse.statusCode == 200) {
-      setState(() {
-        mainPet = getMainPetResponse.pet;
-        _loadingPet = false;
-      });
-    }
-  }
-
-  Future<void> getTotalInfo() async {
-    getWalkTotalInfo? getWalkTotalInfoResponse =
-        await apiWalk.getTotalInfo(user!.petId!);
-    if (getWalkTotalInfoResponse.statusCode == 200) {
-      setState(() {
-        walkInfo = getWalkTotalInfoResponse.totalInfo;
-        _loadingInfo = false;
-      });
-    }
-  }
-
-  Future<void> getIsDone() async {
-    getWalkDone? getWalkDoneResponse = await apiWalk.getIsDone(user!.petId!);
-    if (getWalkDoneResponse.statusCode == 200) {
-      setState(() {
-        done = getWalkDoneResponse.done!;
-        _loadingDone = false;
-      });
-    }
-  }
-
-  @override
-  void initState() {
-    super.initState();
-    getUser();
-  }
+  bool? loadingPet;
+  bool? loadingInfo;
+  bool? loadingDone;
 
   @override
   Widget build(BuildContext context) {
-    if (_loadingPet || _loadingInfo || _loadingDone) {
+    if (loadingPet! || loadingInfo! || loadingDone!) {
       return Container(
           margin: EdgeInsets.fromLTRB(0, 10.h, 0, 10.h),
           child: Column(children: [
@@ -113,7 +59,7 @@ class _pet_walkoutState extends State<pet_walkout> {
                       fontFamily: "Sub",
                       fontSize: 25.sp,
                     ),
-                    done ? "오늘은 산책을 다녀왔어요!" : "아직 산책을 못했어요.."),
+                    done! ? "오늘은 산책을 다녀왔어요!" : "아직 산책을 못했어요.."),
               ),
               Text(
                   style: TextStyle(
